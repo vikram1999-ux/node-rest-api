8e9f1219f6a06577aea4192536c252b4aced138f
vikram mahara <vmahara1999@gmail.com>
2021-02-08 18:49:19 +0530

    simple

---
+++ api/routes/products.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+
+const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
+const ProductsController = require("../controllers/products");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  //reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg or .png files are accepted"), false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
+});
+
+router.get("/", ProductsController.products_get_all);
+
+router.post(
+  "/",
+  checkAuth,
+  upload.single("productImage"),
+  ProductsController.products_create_product
+);
+
+router.get("/:productId", ProductsController.products_get_product);
+
+router.patch(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_update_product
+);
+
+router.delete(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_delete_product
+);
+
+module.exports = router;
